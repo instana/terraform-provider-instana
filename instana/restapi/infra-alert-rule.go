a44b41c0c020df06f46ab0085708261ae5a57d97
Raj Parekh <raj.parekh@ibm.com>
2024-12-16 21:26:05 +0530

    Modify InfraAlertRule model

--- instana/restapi/infra-alert-rule.go
+++ instana/restapi/infra-alert-rule.go
@@ -1,10 +1,10 @@
 package restapi
 
 type InfraAlertRule struct {
-	AlertType              string       `json:"alertType"`
-	MetricName             string       `json:"metricName"`
-	EntityType             string       `json:"entityType"`
-	Aggregation            *Aggregation `json:"aggregation"`
-	CrossSeriesAggregation *Aggregation `json:"crossSeriesAggregation"`
-	Regex                  bool         `json:"regex"`
+	AlertType              string      `json:"alertType"`
+	MetricName             string      `json:"metricName"`
+	EntityType             string      `json:"entityType"`
+	Aggregation            Aggregation `json:"aggregation"`
+	CrossSeriesAggregation Aggregation `json:"crossSeriesAggregation"`
+	Regex                  bool        `json:"regex"`
 }
