efc70f7f9f0a64423e1d858a452bf429b9388349
Dhinesh Sri Raam Marikumar <Dhinesh.Sriraam@ibm.com>
2025-02-21 00:20:37 +0000

    slo-alert api file modifications

--- instana/restapi/slo-alert-config-api.go
+++ instana/restapi/slo-alert-config-api.go
@@ -12,32 +12,40 @@ const (
 
 // SloAlertConfig represents the REST resource of SLO Alert Configuration at Instana
 type SloAlertConfig struct {
-	ID          		string      `json:"id"`
-	Name        		string      `json:"name"`
-	Description 		string     	`json:"description"`
-	Severity    		int      	`json:"severity"`
-	Triggering  		bool      	`json:"triggering"`
-	Enabled    			bool     	`json:"enabled"`
-	AlertType			string		`json:"alertType"`
-	Threshold   		interface{} `json:"threshold"`
-	TimeThreshold  		interface{} `json:"timeThreshold"`
-	SloIds      		[]string    `json:"sloIds"`
-	AlertChannelIds 	[]string 	`json:"alertChannelIds"`
-	CustomPayloadFields []string 	`json:"customPayloadFields"`
+	ID          			string              		`json:"id"`
+	Name        			string              		`json:"name"`
+	Description 			string              		`json:"description"`
+	Severity    			int                 		`json:"severity"`
+	Triggering  			bool                		`json:"triggering"`
+	Enabled    				bool                		`json:"enabled"`
+	AlertType				string              		`json:"alertType"`
+	Threshold   			SloAlertThreshold   		`json:"threshold"` 
+	TimeThreshold  			SloAlertTimeThreshold 		`json:"timeThreshold"` 
+	SloIds      			[]string          	  		`json:"sloIds"`
+	AlertChannelIds 		[]string            		`json:"alertChannelIds"`
+	CustomerPayloadFields 	[]CustomPayloadField[any]	`json:"customPayloadFields"`
 }
 
 type SloAlertThreshold struct {
-	Operator     string        `json:"operator"`
-	Value 		 float64 	   `json:"value"`
+	Operator string  `json:"operator"`
+	Value    float64 `json:"value"`
 }
 
 type SloAlertTimeThreshold struct {
-	Timewindow      int        `json:"timewindow"`
-	expiry			int    	   `json:"expiry"`
+	Timewindow 	int `json:"timewindow"`
+	Expiry     	int `json:"expiry"`
 }
 
 // GetIDForResourcePath implementation of the interface InstanaDataObject
 func (s *SloAlertConfig) GetIDForResourcePath() string {
 	fmt.Fprintln(os.Stderr, ">> GetIDForResourcePath: "+s.ID)
 	return s.ID
+}
+
+func (config *SloAlertConfig) GetCustomerPayloadFields() []CustomPayloadField[any] {
+	return config.CustomerPayloadFields
+}
+
+func (config *SloAlertConfig) SetCustomerPayloadFields(fields []CustomPayloadField[any]) {
+	config.CustomerPayloadFields = fields
 }
