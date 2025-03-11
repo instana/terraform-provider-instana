4ae160885ea9653b9dceb12e5b2caa63ae137522
Dhinesh Sri Raam Marikumar <Dhinesh.Sriraam@ibm.com>
2025-03-11 15:13:29 +0000

    added more tests for SLO SA

--- instana/resource-slo-alert-config_test.go
+++ instana/resource-slo-alert-config_test.go
@@ -10,17 +10,20 @@ import (
 )
 
 const (
-	sloAlertConfigDefinition = "instana_sli_config.example_sli_config"
-	sloAlertID                               = "id"
-	sloAlertName                             = "slo-alert-name"
-	sloAlertDescription                             = "slo-alert-description"
-	sloAlertSeverity                            = 10
-	SloAlertTriggering                           = true
-	sloAlertAlertType                             = "status"
-	sloAlertAlertEnabled                             = true
-	SloAlertThresholdType					= "staticThreshold"
-	SloAlertThresholdOperator							= ">"
-	SloAlertThresholdValue		= -1
+	sloAlertConfigDefinition      = "instana_sli_config.example_sli_config"
+	sloAlertID                    = "id"
+	sloAlertName                  = "slo-alert-name"
+	sloAlertDescription           = "slo-alert-description"
+	sloAlertSeverity              = 10
+	SloAlertTriggering            = true
+	sloAlertAlertType             = "status"
+	sloAlertAlertEnabled          = true
+	SloAlertThresholdType         = "staticThreshold"
+	SloAlertThresholdOperator     = ">"
+	SloAlertThresholdValue        = 0.3
+	SloAlertTimeThresholdWarmUp   = 60000
+	SloAlertTimeThresholdCoolDown = 30000
+
 )
 
 func TestSloAlertConfig(t *testing.T) {
@@ -109,7 +112,10 @@ func (test *sloAlertConfigTest) run(t *testing.T) {
 	t.Run(fmt.Sprintf("%s should have correct resouce name", ResourceInstanaSloAlertConfig), test.createTestResourceShouldHaveResourceName())
 	t.Run(fmt.Sprintf("%s should have one state upgrader", ResourceInstanaSloAlertConfig), test.createTestResourceShouldHaveOneStateUpgrader())
 	t.Run(fmt.Sprintf("%s should have schema version one", ResourceInstanaSloAlertConfig), test.createTestResourceShouldHaveSchemaVersionOne())
-	
+	t.Run("Should require Threshold Values to Match the Assigned Values", test.shouldRequireThresholdMetricsToMatchTheAssignedValues())
+	t.Run("Should require Time Threshold Values to Match the Assigned Values", test.shouldRequireTimeThresholdMetricsToMatchTheAssignedValues())
+
+
 }
 
 
@@ -131,17 +137,13 @@ func (test *sloAlertConfigTest) createTestResourceShouldHaveSchemaVersionOne() f
 	}
 }
 
-func (r *sloAlertConfigTest) shouldRequireMetricConfigurationThresholdToBeGreaterThanOrEqualToZero() func(t *testing.T) {
+func (test *sloAlertConfigTest) shouldRequireThresholdMetricsToMatchTheAssignedValues() func(t *testing.T) {
 	return func(t *testing.T) {
 		testHelper := NewTestHelper[*restapi.SloAlertConfig](t)
 		resourceHandle := NewSloAlertConfigResourceHandle()
 		resourceData := testHelper.CreateEmptyResourceDataForResourceHandle(resourceHandle)
 		resourceData.SetId(sloAlertID)
 		setValueOnResourceData(t, resourceData, SloAlertConfigFieldName, sloAlertName)
-		// setValueOnResourceData(t, resourceData, SliConfigFieldName, sliConfigName)
-		// setValueOnResourceData(t, resourceData, SliConfigFieldName, sliConfigName)
-		// setValueOnResourceData(t, resourceData, SliConfigFieldName, sliConfigName)
-		// setValueOnResourceData(t, resourceData, SliConfigFieldName, sliConfigName)
 
 		thresholdStateObject := []map[string]interface{}{
 			{
@@ -152,7 +154,45 @@ func (r *sloAlertConfigTest) shouldRequireMetricConfigurationThresholdToBeGreate
 		}
 		setValueOnResourceData(t, resourceData, SloAlertConfigFieldThreshold, thresholdStateObject)
 
-		_, metricThresholdIsOK := resourceData.GetOk("threshold.0.value")
-		require.False(t, metricThresholdIsOK)
+		thresholdType, type_ok := resourceData.GetOk("threshold.0.type")
+		thresholdOperator, operator_ok := resourceData.GetOk("threshold.0.operator")
+		thresholdValue, value_ok := resourceData.GetOk("threshold.0.value")
+
+		require.True(t, type_ok, "threshold.0.type should exist")
+		require.True(t, operator_ok, "threshold.0.operator should exist")
+		require.True(t, value_ok, "threshold.0.value should exist")
+
+		require.Equal(t, SloAlertThresholdType, thresholdType, "threshold.type should match set value")	
+		require.Equal(t, SloAlertThresholdOperator, thresholdOperator, "threshold.type should match set value")	
+		require.Equal(t, SloAlertThresholdValue, thresholdValue, "threshold.type should match set value")	
+
+	}
+}
+
+func (test *sloAlertConfigTest) shouldRequireTimeThresholdMetricsToMatchTheAssignedValues() func(t *testing.T) {
+	return func(t *testing.T) {
+		testHelper := NewTestHelper[*restapi.SloAlertConfig](t)
+		resourceHandle := NewSloAlertConfigResourceHandle()
+		resourceData := testHelper.CreateEmptyResourceDataForResourceHandle(resourceHandle)
+		resourceData.SetId(sloAlertID)
+		setValueOnResourceData(t, resourceData, SloAlertConfigFieldName, sloAlertName)
+
+		timeThresholdStateObject := []map[string]interface{}{
+			{
+				SloAlertConfigFieldTimeThresholdWarmUp  : SloAlertTimeThresholdWarmUp,
+				SloAlertConfigFieldTimeThresholdCoolDown: SloAlertTimeThresholdCoolDown,
+			},
+		}
+		setValueOnResourceData(t, resourceData, SloAlertConfigFieldTimeThreshold, timeThresholdStateObject)
+
+		timeThresholdWarmUp, warm_ok := resourceData.GetOk("time_threshold.0.warm_up")
+		timeThresholdCoolDown, cool_ok := resourceData.GetOk("time_threshold.0.cool_down")
+
+		require.True(t, warm_ok, "time_threshold.0.warm_up should exist")
+		require.True(t, cool_ok, "time_threshold.0.cool_down should exist")
+
+		require.Equal(t, SloAlertTimeThresholdWarmUp, timeThresholdWarmUp, "time_threshold.0.warm_up should match set value")	
+		require.Equal(t, SloAlertTimeThresholdCoolDown, timeThresholdCoolDown, "time_threshold.0.cool_down should match set value")	
+
 	}
 }
