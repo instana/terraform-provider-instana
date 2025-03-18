c5d124132851cda5176403b6157c73ddb8c2a9a8
Dhinesh Sri Raam Marikumar <Dhinesh.Sriraam@ibm.com>
2025-03-18 15:42:04 +0000

    updated tests for SLO configs

--- instana/resource-slo-config_test.go
+++ instana/resource-slo-config_test.go
@@ -109,9 +109,7 @@ func (test *sloConfigTest) run(t *testing.T) {
 	t.Run("Application ID Should Be Required For Application Entity", test.applicationIDShouldBeRequiredForApplicationEntity())
 	t.Run("Should Map Valid Slo Application Config To API Object", test.shouldMapApplicationSloConfigToAPIObject())
 	t.Run("Should Map Valid Slo Website Config To API Object", test.shouldMapWebsiteSloConfigToAPIObject())
-
-
-	
+	t.Run("Should Map Valid Slo Synthetic Config To API Object", test.shouldMapSyntheticSloConfigToAPIObject())
 }
 
 func (test *sloConfigTest) createTestResourceShouldHaveResourceName() func(t *testing.T) {
@@ -156,6 +154,35 @@ func (r *sloConfigTest) thresholdShouldBeGreaterThanZero() func(t *testing.T) {
 
 		_, metricThresholdIsOK:= resourceData.GetOk("indicator.0.time_based_latency.0.threshold")
 		require.False(t, metricThresholdIsOK)
+	}
+}
+
+func (r *sloConfigTest) applicationIDShouldBeRequiredForApplicationEntity() func(t *testing.T) {
+	return func(t *testing.T) {
+		testHelper := NewTestHelper[*restapi.SloConfig](t)
+		resourceHandle := NewSloConfigResourceHandle()
+		resourceData := testHelper.CreateEmptyResourceDataForResourceHandle(resourceHandle)
+		resourceData.SetId(sloConfigID)
+
+		setValueOnResourceData(t, resourceData, SloConfigFieldName, sloName)
+		setValueOnResourceData(t, resourceData, SloConfigFieldTarget, sloTarget)
+
+		sloConfigEntityStateObject := []interface{}{
+			map[string]interface{}{
+				SloConfigApplicationEntity: []interface{}{
+					map[string]interface{}{
+						SloConfigFieldBoundaryScope: "ALL",
+						SloConfigFieldApplicationID: nil,
+					},
+				},
+			},
+		}
+
+		setValueOnResourceData(t, resourceData, SloConfigFieldSloEntity, sloConfigEntityStateObject)
+
+		_, applicationIDIsOK := resourceData.GetOk("entity.0.application.0.application_id")
+
+		require.False(t, applicationIDIsOK, "Application ID should be required for application entity")
 	}
 }
 
@@ -221,7 +248,7 @@ func (r *sloConfigTest) shouldMapApplicationSloConfigToAPIObject() func(t *testi
         require.Equal(t, 99.9, apiObject.Target, "Target should match")
         require.Equal(t, []interface{}{"tag1", "tag2"}, apiObject.Tags, "Tags should match")
 
-		// application entity
+		// Validate entity
 		require.NotNil(t, apiObject.Entity, "Entity should not be nil")
 		entity, ok := apiObject.Entity.(restapi.SloApplicationEntity)
 		require.True(t, ok, "Entity should be a SloApplicationEntity")
@@ -252,35 +279,6 @@ func (r *sloConfigTest) shouldMapApplicationSloConfigToAPIObject() func(t *testi
         require.Equal(t, 7, timeWindow.Duration, "Duration should match")
         require.Equal(t, "day", timeWindow.DurationUnit, "DurationUnit should match")
     }
-}
-
-func (r *sloConfigTest) applicationIDShouldBeRequiredForApplicationEntity() func(t *testing.T) {
-	return func(t *testing.T) {
-		testHelper := NewTestHelper[*restapi.SloConfig](t)
-		resourceHandle := NewSloConfigResourceHandle()
-		resourceData := testHelper.CreateEmptyResourceDataForResourceHandle(resourceHandle)
-		resourceData.SetId(sloConfigID)
-
-		setValueOnResourceData(t, resourceData, SloConfigFieldName, sloName)
-		setValueOnResourceData(t, resourceData, SloConfigFieldTarget, sloTarget)
-
-		sloConfigEntityStateObject := []interface{}{
-			map[string]interface{}{
-				SloConfigApplicationEntity: []interface{}{
-					map[string]interface{}{
-						SloConfigFieldBoundaryScope: "ALL",
-						SloConfigFieldApplicationID: nil,
-					},
-				},
-			},
-		}
-
-		setValueOnResourceData(t, resourceData, SloConfigFieldSloEntity, sloConfigEntityStateObject)
-
-		_, applicationIDIsOK := resourceData.GetOk("entity.0.application.0.application_id")
-
-		require.False(t, applicationIDIsOK, "Application ID should be required for application entity")
-	}
 }
 
 func (r *sloConfigTest) shouldMapWebsiteSloConfigToAPIObject() func(t *testing.T) {
@@ -352,6 +350,92 @@ func (r *sloConfigTest) shouldMapWebsiteSloConfigToAPIObject() func(t *testing.T
 		require.Equal(t, "web-123", *entity.WebsiteId, "WebsiteID should match")
 		require.NotNil(t, entity.BeaconType, "BeaconType should not be nil")
 		require.Equal(t, "httpRequest", *entity.BeaconType, "BeaconType should match")
+
+        // Validate indicator
+        require.NotNil(t, apiObject.Indicator, "Indicator should not be nil")
+        indicator, ok := apiObject.Indicator.(restapi.SloTimeBasedLatencyIndicator)
+        require.True(t, ok, "Indicator should be a SloTimeBasedLatencyIndicator")
+        require.Equal(t, instana.SloConfigAPIIndicatorBlueprintLatency, indicator.Blueprint, "Blueprint should match")
+        require.Equal(t, instana.SloConfigAPIIndicatorMeasurementTypeTimeBased, indicator.Type, "Type should match")
+        require.Equal(t, 20.0, indicator.Threshold, "Threshold should match")
+        require.Equal(t, "MEAN", indicator.Aggregation, "Aggregation should match")
+
+        // Validate time window
+        require.NotNil(t, apiObject.TimeWindow, "TimeWindow should not be nil")
+        timeWindow, ok := apiObject.TimeWindow.(restapi.SloRollingTimeWindow)
+        require.True(t, ok, "TimeWindow should be a SloRollingTimeWindow")
+        require.Equal(t, instana.SloConfigRollingTimeWindow, timeWindow.Type, "Type should match")
+        require.Equal(t, 7, timeWindow.Duration, "Duration should match")
+        require.Equal(t, "day", timeWindow.DurationUnit, "DurationUnit should match")
+    }
+}
+
+func (r *sloConfigTest) shouldMapSyntheticSloConfigToAPIObject() func(t *testing.T) {
+    return func(t *testing.T) {
+        testHelper := NewTestHelper[*restapi.SloConfig](t)
+        resourceHandle := NewSloConfigResourceHandle()
+        resourceData := testHelper.CreateEmptyResourceDataForResourceHandle(resourceHandle)
+        resourceData.SetId("test-slo-id")
+
+        setValueOnResourceData(t, resourceData, instana.SloConfigFieldName, "Test SLO Config")
+        setValueOnResourceData(t, resourceData, instana.SloConfigFieldTarget, 99.9)
+        setValueOnResourceData(t, resourceData, instana.SloConfigFieldTags, []interface{}{"tag1", "tag2"})
+
+		// entity
+        setValueOnResourceData(t, resourceData, instana.SloConfigFieldSloEntity, []interface{}{
+            map[string]interface{}{
+                instana.SloConfigSyntheticEntity: []interface{}{
+                    map[string]interface{}{
+						instana.SloConfigFieldSyntheticTestIDs: []interface{}{"test-id-1", "test-id-2"},
+						SloConfigFieldFilterExpression        : "AND",
+
+                    },
+                },
+            },
+        })
+
+        // Indicator (time_based_latency)
+        setValueOnResourceData(t, resourceData, instana.SloConfigFieldSloIndicator, []interface{}{
+            map[string]interface{}{
+                instana.SloConfigTimeBasedLatencyIndicator: []interface{}{
+                    map[string]interface{}{
+                        "threshold":   20.0,
+                        "aggregation": "MEAN",
+                    },
+                },
+            },
+        })
+
+        // Time window (rolling)
+        setValueOnResourceData(t, resourceData, instana.SloConfigFieldSloTimeWindow, []interface{}{
+            map[string]interface{}{
+                "rolling": []interface{}{
+                    map[string]interface{}{
+                        "duration":      7,
+                        "duration_unit": "day",
+                    },
+                },
+            },
+        })
+
+        apiObject, err := resourceHandle.MapStateToDataObject(resourceData)
+        require.NoError(t, err, "MapStateToDataObject should not return an error for valid input")
+
+        // Log for debugging
+        // t.Log("API Object", apiObject)
+
+        require.Equal(t, "test-slo-id", apiObject.ID, "ID should match")
+        require.Equal(t, "Test SLO Config", apiObject.Name, "Name should match")
+        require.Equal(t, 99.9, apiObject.Target, "Target should match")
+        require.Equal(t, []interface{}{"tag1", "tag2"}, apiObject.Tags, "Tags should match")
+
+		// Validate entity
+		require.NotNil(t, apiObject.Entity, "Entity should not be nil")
+		entity, ok := apiObject.Entity.(restapi.SloSyntheticEntity)
+		require.True(t, ok, "Entity should be a SloSyntheticEntity")
+		require.Equal(t, instana.SloConfigSyntheticEntity, entity.Type, "Entity Type should be synthetic")
+		require.NotNil(t, entity.SyntheticTestIDs, "SyntheticTestIDs should not be nil")
+		require.Equal(t, []interface{}{"test-id-1", "test-id-2"}, entity.SyntheticTestIDs, "SyntheticTestIDs should match expected values")
 
         // Validate indicator
         require.NotNil(t, apiObject.Indicator, "Indicator should not be nil")
