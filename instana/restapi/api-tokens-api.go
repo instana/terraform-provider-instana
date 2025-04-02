f4a8f36f3102e3b37806ee2af2377287ab03d0c3
Raj Parekh <raj.parekh@ibm.com>
2025-04-02 13:18:13 +0200

    Update APIToken model with latest alerting permission

--- instana/restapi/api-tokens-api.go
+++ instana/restapi/api-tokens-api.go
@@ -5,35 +5,42 @@ const APITokensResourcePath = SettingsBasePath + "/api-tokens"
 
 // APIToken is the representation of a API Token in Instana
 type APIToken struct {
-	ID                                   string `json:"id"`
-	AccessGrantingToken                  string `json:"accessGrantingToken"`
-	InternalID                           string `json:"internalId"`
-	Name                                 string `json:"name"`
-	CanConfigureServiceMapping           bool   `json:"canConfigureServiceMapping"`
-	CanConfigureEumApplications          bool   `json:"canConfigureEumApplications"`
-	CanConfigureMobileAppMonitoring      bool   `json:"canConfigureMobileAppMonitoring"` //NEW
-	CanConfigureUsers                    bool   `json:"canConfigureUsers"`
-	CanInstallNewAgents                  bool   `json:"canInstallNewAgents"`
-	CanConfigureIntegrations             bool   `json:"canConfigureIntegrations"`
-	CanConfigureCustomAlerts             bool   `json:"canConfigureCustomAlerts"`
-	CanConfigureAPITokens                bool   `json:"canConfigureApiTokens"`
-	CanConfigureAgentRunMode             bool   `json:"canConfigureAgentRunMode"`
-	CanViewAuditLog                      bool   `json:"canViewAuditLog"`
-	CanConfigureAgents                   bool   `json:"canConfigureAgents"`
-	CanConfigureAuthenticationMethods    bool   `json:"canConfigureAuthenticationMethods"`
-	CanConfigureApplications             bool   `json:"canConfigureApplications"`
-	CanConfigureTeams                    bool   `json:"canConfigureTeams"`
-	CanConfigureReleases                 bool   `json:"canConfigureReleases"`
-	CanConfigureLogManagement            bool   `json:"canConfigureLogManagement"`
-	CanCreatePublicCustomDashboards      bool   `json:"canCreatePublicCustomDashboards"`
-	CanViewLogs                          bool   `json:"canViewLogs"`
-	CanViewTraceDetails                  bool   `json:"canViewTraceDetails"`
-	CanConfigureSessionSettings          bool   `json:"canConfigureSessionSettings"`
-	CanConfigureServiceLevelIndicators   bool   `json:"canConfigureServiceLevelIndicators"`
-	CanConfigureGlobalAlertPayload       bool   `json:"canConfigureGlobalAlertPayload"`
-	CanConfigureGlobalAlertConfigs       bool   `json:"canConfigureGlobalAlertConfigs"`
-	CanViewAccountAndBillingInformation  bool   `json:"canViewAccountAndBillingInformation"`
-	CanEditAllAccessibleCustomDashboards bool   `json:"canEditAllAccessibleCustomDashboards"`
+	ID                                       string `json:"id"`
+	AccessGrantingToken                      string `json:"accessGrantingToken"`
+	InternalID                               string `json:"internalId"`
+	Name                                     string `json:"name"`
+	CanConfigureServiceMapping               bool   `json:"canConfigureServiceMapping"`
+	CanConfigureEumApplications              bool   `json:"canConfigureEumApplications"`
+	CanConfigureMobileAppMonitoring          bool   `json:"canConfigureMobileAppMonitoring"` //NEW
+	CanConfigureUsers                        bool   `json:"canConfigureUsers"`
+	CanInstallNewAgents                      bool   `json:"canInstallNewAgents"`
+	CanConfigureIntegrations                 bool   `json:"canConfigureIntegrations"`
+	CanConfigureEventsAndAlerts              bool   `json:"canConfigureEventsAndAlerts"`
+	CanConfigureMaintenanceWindows           bool   `json:"canConfigureMaintenanceWindows"`
+	CanConfigureApplicationSmartAlerts       bool   `json:"canConfigureApplicationSmartAlerts"`
+	CanConfigureWebsiteSmartAlerts           bool   `json:"canConfigureWebsiteSmartAlerts"`
+	CanConfigureMobileAppSmartAlerts         bool   `json:"canConfigureMobileAppSmartAlerts"`
+	CanConfigureAPITokens                    bool   `json:"canConfigureApiTokens"`
+	CanConfigureAgentRunMode                 bool   `json:"canConfigureAgentRunMode"`
+	CanViewAuditLog                          bool   `json:"canViewAuditLog"`
+	CanConfigureAgents                       bool   `json:"canConfigureAgents"`
+	CanConfigureAuthenticationMethods        bool   `json:"canConfigureAuthenticationMethods"`
+	CanConfigureApplications                 bool   `json:"canConfigureApplications"`
+	CanConfigureTeams                        bool   `json:"canConfigureTeams"`
+	CanConfigureReleases                     bool   `json:"canConfigureReleases"`
+	CanConfigureLogManagement                bool   `json:"canConfigureLogManagement"`
+	CanCreatePublicCustomDashboards          bool   `json:"canCreatePublicCustomDashboards"`
+	CanViewLogs                              bool   `json:"canViewLogs"`
+	CanViewTraceDetails                      bool   `json:"canViewTraceDetails"`
+	CanConfigureSessionSettings              bool   `json:"canConfigureSessionSettings"`
+	CanConfigureServiceLevelIndicators       bool   `json:"canConfigureServiceLevelIndicators"`
+	CanConfigureGlobalAlertPayload           bool   `json:"canConfigureGlobalAlertPayload"`
+	CanConfigureGlobalApplicationSmartAlerts bool   `json:"canConfigureGlobalApplicationSmartAlerts"`
+	CanConfigureGlobalSyntheticSmartAlerts   bool   `json:"canConfigureGlobalSyntheticSmartAlerts"`
+	CanConfigureGlobalInfraSmartAlerts       bool   `json:"canConfigureGlobalInfraSmartAlerts"`
+	CanConfigureGlobalLogSmartAlerts         bool   `json:"canConfigureGlobalLogSmartAlerts"`
+	CanViewAccountAndBillingInformation      bool   `json:"canViewAccountAndBillingInformation"`
+	CanEditAllAccessibleCustomDashboards     bool   `json:"canEditAllAccessibleCustomDashboards"`
 }
 
 // GetIDForResourcePath implemention of the interface InstanaDataObject
