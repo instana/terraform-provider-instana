9b170896b1d8b50dd262b0bd6baba42a5f78c799
Eugen Postea <epostea@gmail.com>
2025-05-09 17:44:55 -0400

    Fix unit tests

--- instana/provider_test.go
+++ instana/provider_test.go
@@ -29,7 +29,7 @@ func TestProviderShouldContainValidSchemaDefinition(t *testing.T) {
 func TestProviderShouldContainValidResourceDefinitions(t *testing.T) {
 	config := Provider()
 
-	assert.Equal(t, 16, len(config.ResourcesMap))
+	assert.Equal(t, 17, len(config.ResourcesMap))
 
 	assert.NotNil(t, config.ResourcesMap[ResourceInstanaAPIToken])
 	assert.NotNil(t, config.ResourcesMap[ResourceInstanaApplicationConfig])
@@ -47,14 +47,16 @@ func TestProviderShouldContainValidResourceDefinitions(t *testing.T) {
 	assert.NotNil(t, config.ResourcesMap[ResourceInstanaCustomEventSpecification])
 	assert.NotNil(t, config.ResourcesMap[ResourceInstanaAlertingChannel])
 	assert.NotNil(t, config.ResourcesMap[ResourceInstanaAlertingConfig])
+	assert.NotNil(t, config.ResourcesMap[ResourceInstanaAutomationAction])
 }
 
 func TestProviderShouldContainValidDataSourceDefinitions(t *testing.T) {
 	config := Provider()
 
-	assert.Equal(t, 3, len(config.DataSourcesMap))
+	assert.Equal(t, 4, len(config.DataSourcesMap))
 
 	assert.NotNil(t, config.DataSourcesMap[DataSourceBuiltinEvent])
 	assert.NotNil(t, config.DataSourcesMap[DataSourceSyntheticLocation])
 	assert.NotNil(t, config.DataSourcesMap[DataSourceAlertingChannel])
+	assert.NotNil(t, config.DataSourcesMap[DataSourceAutomationAction])
 }
