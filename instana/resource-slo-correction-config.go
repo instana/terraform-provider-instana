a246e4053ee9e3fece0a760ee1d407654b37135c
Dhinesh Sri Raam Marikumar <Dhinesh.Sriraam@ibm.com>
2025-06-19 11:44:35 +0100

    Adding SLO  Correction Config

---
+++ instana/resource-slo-correction-config.go
@@ -0,0 +1,130 @@
+package instana
+
+import (
+	// "fmt"
+	// "strconv"
+	// "context"
+	"github.com/gessnerfl/terraform-provider-instana/instana/restapi"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+const ResourceInstanaSloCorrectionConfig = "instana_slo_correction_config"
+
+const (
+	// Slo Correction Config Field names for Terraform
+	SloCorrectionConfigFieldName                    = "name"
+	SloCorrectionConfigFieldDescription             = "description"
+	SloCorrectionConfigFieldActive                  = "active"
+	SloCorrectionConfigFieldScheduling              = "scheduling"
+	SloCorrectionConfigFieldSloIds                  = "slo_ids"
+	SloCorrectionConfigFieldTags                    = "tags"
+	SloCorrectionConfigFieldSchedulingStartTime     = "start_time"
+	SloCorrectionConfigFieldSchedulingDuration      = "duration"
+	SloCorrectionConfigFieldSchedulingDurationUnit  = "duration_unit"
+	SloCorrectionConfigFieldSchedulingRecurrentRule = "recurrent_rule"
+)
+
+var (
+	// SloCorrectionConfigName defines the schema for the 'name' field of instana_slo_correction_config
+	SloCorrectionConfigName = &schema.Schema{
+		Type:         schema.TypeString,
+		Required:     true,
+		ValidateFunc: validation.StringLenBetween(0, 256),
+		Description:  "The name of the SLO Correction config.",
+	}
+
+	// SloCorrectionConfigFullName defines the schema for the computed 'full_name' field
+	SloCorrectionConfigFullName = &schema.Schema{
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "The full name of the SLO Correction config, computed based on provider-level default_name_prefix and default_name_suffix.",
+	}
+
+	// SloCorrectionConfigDescription defines the schema for the 'description' field
+	SloCorrectionConfigDescription = &schema.Schema{
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: "The description of the SLO Correction config.",
+	}
+
+	// SloCorrectionConfigActive defines the schema for the 'active' field
+	SloCorrectionConfigActive = &schema.Schema{
+		Type:        schema.TypeBool,
+		Required:    true,
+		Description: "Indicates whether the Correction config is active.",
+	}
+
+	// SloCorrectionConfigScheduling defines the schema for the 'scheduling' field
+	SloCorrectionConfigScheduling = &schema.Schema{
+		Type:        schema.TypeList,
+		Required:    true,
+		Description: "Scheduling configuration for the SLO Correction config.",
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				SloCorrectionConfigFieldSchedulingStartTime: {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "The start time of the scheduling in Unix timestamp in milliseconds.",
+				},
+				SloCorrectionConfigFieldSchedulingDuration: {
+					Type:        schema.TypeInt,
+					Required:    true,
+					Description: "The duration of the scheduling in the specified unit.",
+				},
+				SloCorrectionConfigFieldSchedulingDurationUnit: {
+					Type:         schema.TypeString,
+					Required:     true,
+					Description:  "The unit of the duration (e.g.,'MINUTE' 'HOUR', 'DAY').",
+					ValidateFunc: validation.StringInSlice([]string{"MINUTE", "HOUR", "DAY"}, true),
+				},
+				SloCorrectionConfigFieldSchedulingRecurrentRule: {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Description: "Recurrent rule for scheduling, if applicable.",
+				},
+			},
+		},
+	}
+
+	// SloCorrectionConfigSloIds defines the schema for the 'slo_ids' field
+	SloCorrectionConfigSloIds = &schema.Schema{
+		Type:        schema.TypeSet,
+		Required:    true,
+		Description: "A set of SLO IDs that this correction config applies to.",
+		Elem: &schema.Schema{
+			Type: schema.TypeString,
+		},
+	}
+
+	// SloCorrectionConfigTags defines the schema for the 'tags' field
+	SloCorrectionConfigTags = &schema.Schema{
+		Type:        schema.TypeSet,
+		Optional:    true,
+		Description: "A list of tags to be associated with the SLO Correction config.",
+		Elem: &schema.Schema{
+			Type: schema.TypeString,
+		},
+	}
+)
+
+// NewSloCorrectionConfigResourceHandle creates a new ResourceHandle for SLO Correction Config
+func NewSloCorrectionConfigResourceHandle() ResourceHandle[*restapi.SloCorrectionConfig] {
+	resource := &sloCorrectionConfigResource{
+		metaData: ResourceMetaData{
+			ResourceName: ResourceInstanaSloCorrectionConfig,
+			Schema: map[string]*schema.Schema{
+				SloCorrectionConfigFieldName:        SloCorrectionConfigName,
+				SloCorrectionConfigFieldDescription: SloCorrectionConfigDescription,
+				SloCorrectionConfigFieldActive:      SloCorrectionConfigActive,
+				SloCorrectionConfigFieldScheduling:  SloCorrectionConfigScheduling,
+				SloCorrectionConfigFieldSloIds:      SloCorrectionConfigSloIds,
+				SloCorrectionConfigFieldTags:        SloCorrectionConfigTags,
+			},
+			SchemaVersion:    1,
+			CreateOnly:       false,
+			SkipIDGeneration: true,
+		},
+	}
+	return resource
+}
---
+++ instana/restapi/slo-correction-config-api.go
@@ -0,0 +1,43 @@
+package restapi
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	//SloCorrectionConfigResourcePath path to slo correction config resource of Instana RESTful API
+	SloCorrectionConfigResourcePath = SettingsBasePath + "/correction"
+)
+
+// SloCorrectionConfig represents the REST resource of SLO Correction Configuration at Instana
+type SloCorrectionConfig struct {
+	ID          string      `json:"id"`
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Active      bool        `json:"active"`
+	Scheduling  Scheduling  `json:"scheduling"`
+	SloIds      []string    `json:"sloIds"`
+	Tags        []string    `json:"tags"`
+}
+
+type DurationUnit string
+
+const (
+	DurationUnitMinute DurationUnit = "MINUTE"
+	DurationUnitHour   DurationUnit = "HOUR"
+	DurationUnitDay    DurationUnit = "DAY"
+)
+
+type Scheduling struct {
+	StartTime     int64       `json:"startTime"` // Unix timestamp in milliseconds
+	Duration      int         `json:"duration"`
+	DurationUnit  DurationUnit `json:"durationUnit"`
+	RecurrentRule string      `json:"recurrentRule,omitempty"`
+}
+
+// GetIDForResourcePath implementation of the interface InstanaDataObject
+func (s *SloCorrectionConfig) GetIDForResourcePath() string {
+	fmt.Fprintln(os.Stderr, ">> GetIDForResourcePath: "+s.ID)
+	return s.ID
+}
