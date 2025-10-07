0758ea1b022ed6f9242f338f889efd99494ef80f
Georgekutty Joseph <georgekutty.joseph1@ibm.com>
2025-10-07 18:32:20 +0530

    migration initial commit

---
+++ instana/provider_framework_test.go
@@ -0,0 +1,14 @@
+package instana
+
+import (
+	"testing"
+)
+
+func TestProviderFramework(t *testing.T) {
+	provider := New("test")()
+	if provider == nil {
+		t.Fatal("provider is nil")
+	}
+}
+
+// Made with Bob
---
+++ instana/resource-alerting-config-framework.go
@@ -0,0 +1,256 @@
+package instana
+
+import (
+	"context"
+	"strings"
+
+	"github.com/gessnerfl/terraform-provider-instana/instana/restapi"
+	"github.com/gessnerfl/terraform-provider-instana/tfutils"
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// ResourceInstanaAlertingConfigFramework the name of the terraform-provider-instana resource to manage alerting configurations
+const ResourceInstanaAlertingConfigFramework = "alerting_config"
+
+// AlertingConfigModel represents the data model for the alerting configuration resource
+type AlertingConfigModel struct {
+	ID                    types.String `tfsdk:"id"`
+	AlertName             types.String `tfsdk:"alert_name"`
+	IntegrationIDs        types.Set    `tfsdk:"integration_ids"`
+	EventFilterQuery      types.String `tfsdk:"event_filter_query"`
+	EventFilterEventTypes types.Set    `tfsdk:"event_filter_event_types"`
+	EventFilterRuleIDs    types.Set    `tfsdk:"event_filter_rule_ids"`
+	CustomPayloadFields   types.Map    `tfsdk:"custom_payload_field"`
+}
+
+// NewAlertingConfigResourceHandleFramework creates the resource handle for Alerting Configuration
+func NewAlertingConfigResourceHandleFramework() ResourceHandleFramework[*restapi.AlertingConfiguration] {
+	return &alertingConfigResourceFramework{
+		metaData: ResourceMetaDataFramework{
+			ResourceName: ResourceInstanaAlertingConfigFramework,
+			Schema: schema.Schema{
+				Description: "This resource manages alerting configurations in Instana.",
+				Attributes: map[string]schema.Attribute{
+					"id": schema.StringAttribute{
+						Computed:    true,
+						Description: "The ID of the alerting configuration.",
+						PlanModifiers: []planmodifier.String{
+							stringplanmodifier.UseStateForUnknown(),
+						},
+					},
+					AlertingConfigFieldAlertName: schema.StringAttribute{
+						Required:    true,
+						Description: "Configures the alert name of the alerting configuration",
+						Validators: []validator.String{
+							stringvalidator.LengthBetween(1, 256),
+						},
+					},
+					AlertingConfigFieldIntegrationIds: schema.SetAttribute{
+						Required:    true,
+						Description: "Configures the list of Integration IDs (Alerting Channels).",
+						ElementType: types.StringType,
+						Validators: []validator.Set{
+							setvalidator.SizeBetween(0, 1024),
+						},
+					},
+					AlertingConfigFieldEventFilterQuery: schema.StringAttribute{
+						Optional:    true,
+						Description: "Configures a filter query to to filter rules or event types for a limited set of entities",
+						Validators: []validator.String{
+							stringvalidator.LengthBetween(0, 2048),
+						},
+					},
+					AlertingConfigFieldEventFilterEventTypes: schema.SetAttribute{
+						Optional:    true,
+						Description: "Configures the list of Event Types IDs which should trigger an alert.",
+						ElementType: types.StringType,
+						Validators: []validator.Set{
+							setvalidator.ValueStringsAre(
+								stringvalidator.OneOf(supportedEventTypes...),
+							),
+						},
+						PlanModifiers: []planmodifier.Set{
+							setplanmodifier.RequiresReplace(),
+						},
+					},
+					AlertingConfigFieldEventFilterRuleIDs: schema.SetAttribute{
+						Optional:    true,
+						Description: "Configures the list of Rule IDs which should trigger an alert.",
+						ElementType: types.StringType,
+						Validators: []validator.Set{
+							setvalidator.SizeBetween(0, 1024),
+						},
+						PlanModifiers: []planmodifier.Set{
+							setplanmodifier.RequiresReplace(),
+						},
+					},
+					DefaultCustomPayloadFieldsName: schema.MapAttribute{
+						Optional:    true,
+						Description: "Custom payload fields for the alerting configuration.",
+						ElementType: types.StringType,
+					},
+				},
+				Blocks: map[string]schema.Block{},
+			},
+			SchemaVersion: 2,
+		},
+	}
+}
+
+type alertingConfigResourceFramework struct {
+	metaData ResourceMetaDataFramework
+}
+
+func (r *alertingConfigResourceFramework) MetaData() *ResourceMetaDataFramework {
+	return &r.metaData
+}
+
+func (r *alertingConfigResourceFramework) GetRestResource(api restapi.InstanaAPI) restapi.RestResource[*restapi.AlertingConfiguration] {
+	return api.AlertingConfigurations()
+}
+
+func (r *alertingConfigResourceFramework) SetComputedFields(_ context.Context, _ *tfsdk.State) diag.Diagnostics {
+	return nil
+}
+
+func (r *alertingConfigResourceFramework) UpdateState(ctx context.Context, state *tfsdk.State, config *restapi.AlertingConfiguration) diag.Diagnostics {
+	// First, set the ID directly since it's a special field
+	diags := state.SetAttribute(ctx, path.Root("id"), types.StringValue(config.ID))
+	if diags.HasError() {
+		return diags
+	}
+
+	// Use UpdateStatePlugin to set all other fields
+	err := tfutils.UpdateStatePlugin(ctx, state, map[string]interface{}{
+		AlertingConfigFieldAlertName:             config.AlertName,
+		AlertingConfigFieldIntegrationIds:        config.IntegrationIDs,
+		AlertingConfigFieldEventFilterQuery:      *config.EventFilteringConfiguration.Query,
+		AlertingConfigFieldEventFilterEventTypes: r.convertEventTypesToHarmonizedStringRepresentation(config.EventFilteringConfiguration.EventTypes),
+		AlertingConfigFieldEventFilterRuleIDs:    config.EventFilteringConfiguration.RuleIDs,
+		DefaultCustomPayloadFieldsName:           mapCustomPayloadFieldsToSchema(config),
+	})
+
+	if err != nil {
+		return diag.Diagnostics{
+			diag.NewErrorDiagnostic(
+				"Error updating state",
+				err.Error(),
+			),
+		}
+	}
+
+	return nil
+}
+
+func (r *alertingConfigResourceFramework) MapStateToDataObject(ctx context.Context, state *tfsdk.State) (*restapi.AlertingConfiguration, diag.Diagnostics) {
+	var diags diag.Diagnostics
+	var model AlertingConfigModel
+
+	// Get current state
+	diags.Append(state.Get(ctx, &model)...)
+	if diags.HasError() {
+		return nil, diags
+	}
+
+	// Map ID
+	id := ""
+	if !model.ID.IsNull() {
+		id = model.ID.ValueString()
+	}
+
+	// Map alert name
+	alertName := model.AlertName.ValueString()
+
+	// Map integration IDs
+	var integrationIDs []string
+	if !model.IntegrationIDs.IsNull() {
+		diags.Append(model.IntegrationIDs.ElementsAs(ctx, &integrationIDs, false)...)
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	// Map event filter query
+	var query *string
+	if !model.EventFilterQuery.IsNull() {
+		queryStr := model.EventFilterQuery.ValueString()
+		query = &queryStr
+	}
+
+	// Map event filter event types
+	var eventTypeStrs []string
+	if !model.EventFilterEventTypes.IsNull() {
+		diags.Append(model.EventFilterEventTypes.ElementsAs(ctx, &eventTypeStrs, false)...)
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+	eventTypes := r.readEventTypesFromStrings(eventTypeStrs)
+
+	// Map event filter rule IDs
+	var ruleIDs []string
+	if !model.EventFilterRuleIDs.IsNull() {
+		diags.Append(model.EventFilterRuleIDs.ElementsAs(ctx, &ruleIDs, false)...)
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	// Map custom payload fields
+	var customerPayloadFields []restapi.CustomPayloadField[any]
+	if !model.CustomPayloadFields.IsNull() {
+		var err error
+		customerPayloadFields, err = mapCustomPayloadFieldsFromSchemaWhenKeyExists(model.CustomPayloadFields)
+		if err != nil {
+			return nil, diag.Diagnostics{
+				diag.NewErrorDiagnostic(
+					"Error updating state",
+					err.Error(),
+				),
+			}
+		}
+
+	}
+
+	return &restapi.AlertingConfiguration{
+		ID:             id,
+		AlertName:      alertName,
+		IntegrationIDs: integrationIDs,
+		EventFilteringConfiguration: restapi.EventFilteringConfiguration{
+			Query:      query,
+			RuleIDs:    ruleIDs,
+			EventTypes: eventTypes,
+		},
+		CustomerPayloadFields: customerPayloadFields,
+	}, diags
+}
+
+func (r *alertingConfigResourceFramework) convertEventTypesToHarmonizedStringRepresentation(input []restapi.AlertEventType) []string {
+	result := make([]string, len(input))
+	for i, v := range input {
+		value := strings.ToLower(string(v))
+		result[i] = value
+	}
+	return result
+}
+
+func (r *alertingConfigResourceFramework) readEventTypesFromStrings(input []string) []restapi.AlertEventType {
+	result := make([]restapi.AlertEventType, len(input))
+	for i, v := range input {
+		value := strings.ToLower(v)
+		result[i] = restapi.AlertEventType(value)
+	}
+	return result
+}
+
+// Made with Bob
