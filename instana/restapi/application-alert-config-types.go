31233f51da1db3e2cb006bce4b9b90385d662933
Georgekutty Joseph <georgekutty.joseph1@ibm.com>
2025-10-21 21:23:07 +0530

    Revert "threshold update"
    This reverts commit 93f1a3b2c249cf805fd3cb0cf3156ebd1a0113a0.

--- instana/restapi/application-alert-config-types.go
+++ instana/restapi/application-alert-config-types.go
@@ -2,9 +2,14 @@ package restapi
 
 // ApplicationAlertRuleWithThresholds represents a rule with multiple thresholds and severity levels
 type ApplicationAlertRuleWithThresholds struct {
-	Rule              *ApplicationAlertRule           `json:"rule"`
-	ThresholdOperator string                          `json:"thresholdOperator"`
-	Thresholds        map[AlertSeverity]ThresholdRule `json:"thresholds"`
+	Rule              *ApplicationAlertRule     `json:"rule"`
+	ThresholdOperator string                    `json:"thresholdOperator"`
+	Thresholds        map[string]ThresholdValue `json:"thresholds"`
+}
+
+// ThresholdValue represents a threshold value for a specific severity level
+type ThresholdValue struct {
+	Value float64 `json:"value"`
 }
 
 // Made with Bob
