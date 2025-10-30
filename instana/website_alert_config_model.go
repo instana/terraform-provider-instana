6dbf27f62bb37cf67792ac57a273449cb267abbe
Georgekutty Joseph <georgekutty.joseph1@ibm.com>
2025-10-30 19:33:56 +0530

    website alert

--- instana/website_alert_config_model.go
+++ instana/website_alert_config_model.go
@@ -1,4 +1,4 @@
-package tf_framework
+package instana
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -6,35 +6,55 @@ import (
 
 // WebsiteAlertConfigModel represents the data model for the Website Alert Config resource
 type WebsiteAlertConfigModel struct {
-	ID                  types.String `tfsdk:"id"`
-	Name                types.String `tfsdk:"name"`
-	Description         types.String `tfsdk:"description"`
-	Severity            types.String `tfsdk:"severity"`
-	Triggering          types.Bool   `tfsdk:"triggering"`
-	WebsiteID           types.String `tfsdk:"website_id"`
-	TagFilter           types.String `tfsdk:"tag_filter"`
-	AlertChannelIDs     types.Set    `tfsdk:"alert_channel_ids"`
-	Granularity         types.Int64  `tfsdk:"granularity"`
-	CustomPayloadFields types.Set    `tfsdk:"custom_payload_fields"`
-	Rule                types.List   `tfsdk:"rule"`
-	Threshold           types.Object `tfsdk:"threshold"`
-	TimeThreshold       types.List   `tfsdk:"time_threshold"`
+	ID                  types.String                   `tfsdk:"id"`
+	Name                types.String                   `tfsdk:"name"`
+	Description         types.String                   `tfsdk:"description"`
+	Severity            types.String                   `tfsdk:"severity"`
+	Triggering          types.Bool                     `tfsdk:"triggering"`
+	WebsiteID           types.String                   `tfsdk:"website_id"`
+	TagFilter           types.String                   `tfsdk:"tag_filter"`
+	AlertChannelIDs     types.Set                      `tfsdk:"alert_channel_ids"`
+	Granularity         types.Int64                    `tfsdk:"granularity"`
+	CustomPayloadFields types.Set                      `tfsdk:"custom_payload_fields"`
+	Rule                WebsiteAlertRuleModel          `tfsdk:"rule"`
+	Threshold           types.Object                   `tfsdk:"threshold"`
+	TimeThreshold       types.List                     `tfsdk:"time_threshold"`
+	Rules               []RuleWithThresholdPluginModel `tfsdk:"rules"`
 }
+
+type RuleWithThresholdPluginModel struct {
+	Rule              WebsiteAlertRuleModel `tfsdk:"rule"`
+	ThresholdOperator types.String          `tfsdk:"threshold_operator"`
+	Thresholds        ThresholdPluginModel  `tfsdk:"threshold"`
+}
+
+// // WebsiteAlertRuleModel represents the rule configuration for Website Alert Config
+// type WebsiteAlertRuleModel struct {
+// 	Slowness        types.List `tfsdk:"slowness"`
+// 	SpecificJsError types.List `tfsdk:"specific_js_error"`
+// 	StatusCode      types.List `tfsdk:"status_code"`
+// 	Throughput      types.List `tfsdk:"throughput"`
+// }
 
 // WebsiteAlertRuleModel represents the rule configuration for Website Alert Config
 type WebsiteAlertRuleModel struct {
-	Slowness        types.List `tfsdk:"slowness"`
-	SpecificJsError types.List `tfsdk:"specific_js_error"`
-	StatusCode      types.List `tfsdk:"status_code"`
-	Throughput      types.List `tfsdk:"throughput"`
+	Slowness        *WebsiteAlertRuleConfigModel         `tfsdk:"slowness"`
+	SpecificJsError *WebsiteAlertRuleConfigCompleteModel `tfsdk:"specific_js_error"`
+	StatusCode      *WebsiteAlertRuleConfigCompleteModel `tfsdk:"status_code"`
+	Throughput      *WebsiteAlertRuleConfigModel         `tfsdk:"throughput"`
 }
 
 // WebsiteAlertRuleConfigModel represents the common configuration for Website Alert Rules
-type WebsiteAlertRuleConfigModel struct {
+type WebsiteAlertRuleConfigCompleteModel struct {
 	MetricName  types.String `tfsdk:"metric_name"`
 	Aggregation types.String `tfsdk:"aggregation"`
 	Operator    types.String `tfsdk:"operator"`
 	Value       types.String `tfsdk:"value"`
+}
+
+type WebsiteAlertRuleConfigModel struct {
+	MetricName  types.String `tfsdk:"metric_name"`
+	Aggregation types.String `tfsdk:"aggregation"`
 }
 
 // WebsiteTimeThresholdModel represents the time threshold configuration for Website Alert Config
@@ -70,7 +90,7 @@ type CustomPayloadFieldModel struct {
 }
 
 // ThresholdModel represents the threshold configuration
-type ThresholdModel struct {
+type WebsiteThresholdModel struct {
 	Operator types.String  `tfsdk:"operator"`
 	Value    types.Float64 `tfsdk:"value"`
 }
