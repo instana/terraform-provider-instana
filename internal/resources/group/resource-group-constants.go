c64d11c128363aae8a15f8f3dbcdc36142bcdffd
Georgekutty Joseph <georgekutty.joseph1@ibm.com>
2025-11-13 20:30:43 +0530

    code clean up

--- internal/resources/group/resource-group-constants.go
+++ internal/resources/group/resource-group-constants.go
@@ -5,47 +5,74 @@ const ResourceInstanaGroupFramework = "rbac_group"
 
 //nolint:gosec
 const (
-	//GroupFieldName constant value for the schema field name
+	// Schema field names
+
+	// GroupFieldID constant value for the schema field id
+	GroupFieldID = "id"
+	// GroupFieldName constant value for the schema field name
 	GroupFieldName = "name"
-	//GroupFieldFullName constant value for the schema field full_name
+	// GroupFieldFullName constant value for the schema field full_name
 	GroupFieldFullName = "full_name"
-	//GroupFieldMembers constant value for the schema field members
+	// GroupFieldMembers constant value for the schema field members
 	GroupFieldMembers = "member"
-	//GroupFieldMemberEmail constant value for the schema field email
+	// GroupFieldMemberEmail constant value for the schema field email
 	GroupFieldMemberEmail = "email"
-	//GroupFieldMemberUserID constant value for the schema field user_id
+	// GroupFieldMemberUserID constant value for the schema field user_id
 	GroupFieldMemberUserID = "user_id"
-	//GroupFieldPermissionSet constant value for the schema field permission_set
+	// GroupFieldPermissionSet constant value for the schema field permission_set
 	GroupFieldPermissionSet = "permission_set"
-	//GroupFieldPermissionSetApplicationIDs constant value for the schema field application_ids
+	// GroupFieldPermissionSetApplicationIDs constant value for the schema field application_ids
 	GroupFieldPermissionSetApplicationIDs = "application_ids"
-	//GroupFieldPermissionSetInfraDFQFilter constant value for the schema field infra_dfq_filter
+	// GroupFieldPermissionSetInfraDFQFilter constant value for the schema field infra_dfq_filter
 	GroupFieldPermissionSetInfraDFQFilter = "infra_dfq_filter"
-	//GroupFieldPermissionSetKubernetesClusterUUIDs constant value for the schema field kubernetes_cluster_uuids
+	// GroupFieldPermissionSetKubernetesClusterUUIDs constant value for the schema field kubernetes_cluster_uuids
 	GroupFieldPermissionSetKubernetesClusterUUIDs = "kubernetes_cluster_uuids"
-	//GroupFieldPermissionSetKubernetesNamespaceUIDs constant value for the schema field kubernetes_namespaces_uuids
+	// GroupFieldPermissionSetKubernetesNamespaceUIDs constant value for the schema field kubernetes_namespaces_uuids
 	GroupFieldPermissionSetKubernetesNamespaceUIDs = "kubernetes_namespaces_uuids"
-	//GroupFieldPermissionSetMobileAppIDs constant value for the schema field mobile_app_ids
+	// GroupFieldPermissionSetMobileAppIDs constant value for the schema field mobile_app_ids
 	GroupFieldPermissionSetMobileAppIDs = "mobile_app_ids"
-	//GroupFieldPermissionSetWebsiteIDs constant value for the schema field website_ids
+	// GroupFieldPermissionSetWebsiteIDs constant value for the schema field website_ids
 	GroupFieldPermissionSetWebsiteIDs = "website_ids"
-	//GroupFieldPermissionSetPermissions constant value for the schema field permissions
+	// GroupFieldPermissionSetPermissions constant value for the schema field permissions
 	GroupFieldPermissionSetPermissions = "permissions"
 
 	// Resource description constants
 
-	GroupDescResource                             = "This resource manages RBAC groups in Instana."
-	GroupDescID                                   = "The ID of the group."
-	GroupDescName                                 = "The name of the Group"
-	GroupDescMembers                              = "The members of the group"
-	GroupDescMemberUserID                         = "The user id of the group member"
-	GroupDescMemberEmail                          = "The email address of the group member"
-	GroupDescPermissionSet                        = "The permission set of the group"
-	GroupDescPermissionSetApplicationIDs          = "The scope bindings to restrict access to applications"
-	GroupDescPermissionSetInfraDFQFilter          = "The scope binding for the dynamic filter query to restrict access to infrastructure assets"
-	GroupDescPermissionSetKubernetesClusterUUIDs  = "The scope bindings to restrict access to Kubernetes Clusters"
+	// GroupDescResource description for the group resource
+	GroupDescResource = "This resource manages RBAC groups in Instana."
+	// GroupDescID description for the ID field
+	GroupDescID = "The ID of the group."
+	// GroupDescName description for the name field
+	GroupDescName = "The name of the Group"
+	// GroupDescMembers description for the members field
+	GroupDescMembers = "The members of the group"
+	// GroupDescMemberUserID description for the member user_id field
+	GroupDescMemberUserID = "The user id of the group member"
+	// GroupDescMemberEmail description for the member email field
+	GroupDescMemberEmail = "The email address of the group member"
+	// GroupDescPermissionSet description for the permission_set field
+	GroupDescPermissionSet = "The permission set of the group"
+	// GroupDescPermissionSetApplicationIDs description for the application_ids field
+	GroupDescPermissionSetApplicationIDs = "The scope bindings to restrict access to applications"
+	// GroupDescPermissionSetInfraDFQFilter description for the infra_dfq_filter field
+	GroupDescPermissionSetInfraDFQFilter = "The scope binding for the dynamic filter query to restrict access to infrastructure assets"
+	// GroupDescPermissionSetKubernetesClusterUUIDs description for the kubernetes_cluster_uuids field
+	GroupDescPermissionSetKubernetesClusterUUIDs = "The scope bindings to restrict access to Kubernetes Clusters"
+	// GroupDescPermissionSetKubernetesNamespaceUIDs description for the kubernetes_namespaces_uuids field
 	GroupDescPermissionSetKubernetesNamespaceUIDs = "The scope bindings to restrict access to Kubernetes namespaces"
-	GroupDescPermissionSetMobileAppIDs            = "The scope bindings to restrict access to mobile apps"
-	GroupDescPermissionSetWebsiteIDs              = "The scope bindings to restrict access to websites"
-	GroupDescPermissionSetPermissions             = "The permissions assigned which should be assigned to the users of the group"
+	// GroupDescPermissionSetMobileAppIDs description for the mobile_app_ids field
+	GroupDescPermissionSetMobileAppIDs = "The scope bindings to restrict access to mobile apps"
+	// GroupDescPermissionSetWebsiteIDs description for the website_ids field
+	GroupDescPermissionSetWebsiteIDs = "The scope bindings to restrict access to websites"
+	// GroupDescPermissionSetPermissions description for the permissions field
+	GroupDescPermissionSetPermissions = "The permissions assigned which should be assigned to the users of the group"
+
+	// Default values and special constants
+
+	// DefaultScopeRoleID represents the default scope role ID for empty infra DFQ filters
+	DefaultScopeRoleID = "-1"
+	// EmptyScopeID represents an empty scope ID
+	EmptyScopeID = ""
 )
+
+// Made with Bob
