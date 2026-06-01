eca9e2cc941d0010c3cd8a1ea5d25430383194f2
nicoleyson <nicoleyson@gmail.com>
2026-06-01 12:40:33 -0700

    resolves: https://github.com/instana/terraform-provider-instana/pull/85#discussion_r3336119624 adds stringvalidator.ExactlyOneOf

--- internal/datasources/data-source-rbac-role.go
+++ internal/datasources/data-source-rbac-role.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/instana/instana-go-client/api"
 	"github.com/instana/instana-go-client/client"
@@ -35,24 +38,30 @@ func (d *RbacRoleDataSource) Metadata(_ context.Context, req datasource.Metadata
 }
 
 func (d *RbacRoleDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-	       resp.Schema = schema.Schema{
-		       Description: RbacRoleDescDataSource,
-		       Attributes: map[string]schema.Attribute{
-			       RbacRoleDataSourceFieldID: schema.StringAttribute{
-				       Description: "ID of the RBAC Role.",
-				       Optional:    true,
-			       },
-			       "name": schema.StringAttribute{
-				       Description: "Name of the RBAC Role.",
-				       Optional:    true,
-			       },
-			       "permissions": schema.SetAttribute{
-				       Description: "Permissions assigned to the RBAC Role.",
-				       Computed:    true,
-				       ElementType: types.StringType,
-			       },
-		       },
-	       }
+	resp.Schema = schema.Schema{
+		Description: RbacRoleDescDataSource,
+		Attributes: map[string]schema.Attribute{
+			RbacRoleDataSourceFieldID: schema.StringAttribute{
+				Description: RbacRoleDescID,
+				Optional:    true,
+				Validators: []validator.String{
+					stringvalidator.ExactlyOneOf(path.MatchRoot(RbacRoleDataSourceFieldID), path.MatchRoot(RbacRoleDataSourceFieldName)),
+				},
+			},
+			RbacRoleDataSourceFieldName: schema.StringAttribute{
+				Description: RbacRoleDescName,
+				Optional:    true,
+				Validators: []validator.String{
+					stringvalidator.ExactlyOneOf(path.MatchRoot(RbacRoleDataSourceFieldID), path.MatchRoot(RbacRoleDataSourceFieldName)),
+				},
+			},
+			RbacRoleDataSourceFieldPermissions: schema.SetAttribute{
+				Description: RbacRoleDescPermissions,
+				Computed:    true,
+				ElementType: types.StringType,
+			},
+		},
+	}
 }
 
 func (d *RbacRoleDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
@@ -73,56 +82,55 @@ func (d *RbacRoleDataSource) Configure(_ context.Context, req datasource.Configu
 }
 
 func (d *RbacRoleDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	       var data RbacRoleDataSourceModel
-	       resp.Diagnostics.Append(req.Config.Get(ctx, &data)...) // Read config into data
-	       if resp.Diagnostics.HasError() {
-		       return
-	       }
+	var data RbacRoleDataSourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-	       id := data.ID.ValueString()
-	       name := data.Name.ValueString()
-	       if id == "" && name == "" {
-		       resp.Diagnostics.AddError("Missing Attribute", "Either 'id' or 'name' must be set.")
-		       return
-	       }
+	id := data.ID.ValueString()
+	name := data.Name.ValueString()
+	if id == "" && name == "" {
+		resp.Diagnostics.AddError("Missing Attribute", RbacRoleErrMissingLookupAttribute)
+		return
+	}
 
 		var role *api.Role
-	       var err error
-	       if id != "" {
-		       role, err = d.instanaAPI.Roles().GetOne(id)
-		       if err != nil {
-			       resp.Diagnostics.AddError("Error reading RBAC Role", fmt.Sprintf("Could not read RBAC Role with ID '%s': %s", id, err))
-			       return
-		       }
-		       if role == nil {
-			       resp.Diagnostics.AddError("Not found", fmt.Sprintf("No RBAC Role found with ID '%s'", id))
-			       return
-		       }
-	       } else {
-		       // Fetch all roles and match by name
-		       roles, err := d.instanaAPI.Roles().GetAll()
-		       if err != nil {
-			       resp.Diagnostics.AddError("Error reading RBAC Roles", fmt.Sprintf("Could not read RBAC Roles: %s", err))
-			       return
-		       }
-		       for _, r := range *roles {
-			       if r.Name == name {
-				       role = r
-				       break
-			       }
-		       }
-		       if role == nil {
-			       resp.Diagnostics.AddError("Not found", fmt.Sprintf("No RBAC Role found with name '%s'", name))
-			       return
-		       }
-	       }
+	var err error
+	if id != "" {
+		role, err = d.instanaAPI.Roles().GetOne(id)
+		if err != nil {
+			resp.Diagnostics.AddError("Error reading RBAC Role", fmt.Sprintf(RbacRoleErrReadByID, id, err))
+			return
+		}
+		if role == nil {
+			resp.Diagnostics.AddError("Not found", fmt.Sprintf(RbacRoleErrNotFoundByID, id))
+			return
+		}
+	} else {
+		roles, err := d.instanaAPI.Roles().GetAll()
+		if err != nil {
+			resp.Diagnostics.AddError("Error reading RBAC Roles", fmt.Sprintf(RbacRoleErrReadAll, err))
+			return
+		}
+		for _, r := range *roles {
+			if r.Name == name {
+				role = r
+				break
+			}
+		}
+		if role == nil {
+			resp.Diagnostics.AddError("Not found", fmt.Sprintf(RbacRoleErrNotFoundByName, name))
+			return
+		}
+	}
 
-	       data.ID = types.StringValue(role.ID)
-	       data.Name = types.StringValue(role.Name)
-	       data.Permissions = make([]types.String, len(role.Permissions))
-	       for i, p := range role.Permissions {
-		       data.Permissions[i] = types.StringValue(p)
-	       }
+	data.ID = types.StringValue(role.ID)
+	data.Name = types.StringValue(role.Name)
+	data.Permissions = make([]types.String, len(role.Permissions))
+	for i, p := range role.Permissions {
+		data.Permissions[i] = types.StringValue(p)
+	}
 
-	       resp.Diagnostics.Append(resp.State.Set(ctx, &data)...) 
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
