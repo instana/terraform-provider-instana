c5b44e2a32461cfb3667f0118f8a62eca6b5d031
BLESSY ELZA BYJU <blessy.elza@ibm.com>
2026-09-01 15:55:55 +0530

    added default for log alert config field

--- internal/resources/logalertconfig/resource-log-alert-config.go
+++ internal/resources/logalertconfig/resource-log-alert-config.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
@@ -159,12 +160,14 @@ func buildRulesSchema() schema.SingleNestedAttribute {
 				Description: LogAlertConfigDescMetricName,
 			},
 			LogAlertConfigFieldAlertType: schema.StringAttribute{
-				Optional:    true,
-				Description: LogAlertConfigDescAlertType,
-				Validators: []validator.String{
-					stringvalidator.OneOf(LogAlertTypeLogCount),
+					Optional:    true,
+					Computed:    true,
+					Default:     stringdefault.StaticString(LogAlertTypeLogCount),
+					Description: LogAlertConfigDescAlertType,
+					Validators: []validator.String{
+						stringvalidator.OneOf(LogAlertTypeLogCount),
+					},
 				},
-			},
 			LogAlertConfigFieldAggregation: schema.StringAttribute{
 				Optional:    true,
 				Computed:    true,
